800a82c50971d68d3c26ba4c6a359a343079df64
Eursukkul <chalermphan172@gmail.com>
2022-08-17 15:32:50 +0700

    Add project files.

---
+++ BasicProgramming/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight, height;
+            //Input
+
+            Console.Write("Input Weight(kg): ");
+            double.TryParse(Console.ReadLine(), out weight);
+            Console.Write("Input Height(cm): ");
+            double.TryParse(Console.ReadLine(), out height);
+
+            //Process
+            //BMI = weight/height^2
+
+            height /= 100;
+            double bmi = weight / Math.Pow(height, 2);
+
+            //Output
+
+            Console.WriteLine("BMI = {0:F2}", bmi);
+            Console.ReadKey();
+        }
+    }
+}
